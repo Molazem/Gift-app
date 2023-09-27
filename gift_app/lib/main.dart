2ff3755be3aa127d5ac3cdcbeb55299264045bd1
Molazem <abdallah.molz.8525@gmail.com>
2023-09-27 14:40:54 +0300

    done

--- gift_app/lib/main.dart
+++ gift_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'signup_page.dart';
 
 void main()  {
-  
+  // This is a nice app
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Gift App',
